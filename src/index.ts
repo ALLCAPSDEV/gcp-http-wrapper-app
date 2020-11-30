efa0f2d9cdc5b15d127063a7a64161c7c73057e4
Matt Riley <matt-riley@users.noreply.github.com>
2020-11-30 19:04:48 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 import { Request, Response, NextFunction } from 'express';
 import { AppCreatorOptions } from '../interfaces/appCreatorOptions';
 import { RequestWithContext } from '../interfaces/requestWithContext';
-import { apiKeyCheck } from './middleware/apiKey';
 import { errorHandler } from './middleware/errorHandler';
 import { logger } from './middleware/logger';
 import { methodCheck } from './middleware/methodCheck';
@@ -10,19 +9,15 @@ import { methodCheck } from './middleware/methodCheck';
 const app = express();
 
 export const createApp = (
-  methods: string[],
   func: (
     req: RequestWithContext,
     res: Response,
     next: NextFunction
   ) => Promise<void>,
-  options?: AppCreatorOptions
+  options: AppCreatorOptions
 ) => async (req: Request, res: Response) => {
   app.use(logger);
-  app.use(methodCheck(methods));
-  if (options?.apiKey) {
-    app.use(apiKeyCheck);
-  }
+  app.use(methodCheck(options.methods));
   app.all('/', async (req: RequestWithContext, res, next: NextFunction) => {
     try {
       await func(req, res, next);
