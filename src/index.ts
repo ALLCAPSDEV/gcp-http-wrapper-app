4b9e4a6a78bcdd2ecc99a848d42ac685c1884692
Matt Riley <matt-riley@users.noreply.github.com>
2020-11-30 18:55:31 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,36 @@
-import express from 'express';
-import { Request, Response } from 'express';
-import { RequestWithContext } from './interfaces/requestWithContext';
-import { errorHandler } from './middleware/errors/errorHandler';
-import { logger } from './middleware/logger/logger';
-
+import * as express from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { AppCreatorOptions } from '../interfaces/appCreatorOptions';
+import { RequestWithContext } from '../interfaces/requestWithContext';
+import { apiKeyCheck } from './middleware/apiKey';
+import { errorHandler } from './middleware/errorHandler';
+import { logger } from './middleware/logger';
 import { methodCheck } from './middleware/methodCheck';
+
 const app = express();
 
 export const createApp = (
   methods: string[],
-  func: (req: Request, res: Response) => Promise<void>
+  func: (
+    req: RequestWithContext,
+    res: Response,
+    next: NextFunction
+  ) => Promise<void>,
+  options?: AppCreatorOptions
 ) => async (req: Request, res: Response) => {
-  app.use((req, res, next) => logger(req as RequestWithContext, res, next));
-  app.all('/', methodCheck(methods));
-  app.all('/', async (req, res) => {
+  app.use(logger);
+  app.use(methodCheck(methods));
+  if (options?.apiKey) {
+    app.use(apiKeyCheck);
+  }
+  app.all('/', async (req: RequestWithContext, res, next: NextFunction) => {
     try {
-      await func(req, res);
+      await func(req, res, next);
     } catch (err) {
-      errorHandler(err, req as RequestWithContext, res);
+      errorHandler(err, req, res);
     }
   });
+  app.use(errorHandler);
 
   return app(req, res);
 };
