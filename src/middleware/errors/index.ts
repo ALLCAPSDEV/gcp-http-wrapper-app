3f183c15589cb88d1d73793134f7d3b4b2de3862
Matt Riley <matt@mattriley.me>
2020-12-04 17:46:30 +0000

    refactor(renamed files): files renamed due to extraction to package

--- src/middleware/errors/index.ts
+++ src/middleware/errors/index.ts
@@ -1,57 +1,23 @@
-import { ErrorMsg } from '../../interfaces/errorMsg';
+import { ErrorReporting } from '@google-cloud/error-reporting';
+import { Response } from 'express';
+import { GeneralError } from './errors';
+import { RequestWithContext } from '../../interfaces/requestWithContext';
 
-const errorCodes = [
-  200,
-  499,
-  500,
-  400,
-  504,
-  404,
-  409,
-  403,
-  401,
-  429,
-  400,
-  409,
-  400,
-  501,
-  500,
-  503,
-  500,
-];
+const errors = new ErrorReporting({
+  reportMode: 'always',
+  reportUnhandledRejections: false,
+});
 
-class GeneralError extends Error {
-  constructor(error: ErrorMsg) {
-    super();
-    this.message = error.message as string;
-  }
-
-  getCode() {
-    if (this instanceof BadRequest) {
-      return 400;
-    }
-    if (this instanceof NotFound) {
-      return 404;
-    }
-    if (this instanceof WrongMethod) {
-      return 405;
-    }
-    if (this instanceof GoogleError) {
-      return this.status;
-    }
-    return 500;
-  }
-}
-
-class BadRequest extends GeneralError {}
-class NotFound extends GeneralError {}
-class WrongMethod extends GeneralError {}
-class GoogleError extends GeneralError {
-  status: number;
-  constructor(error: ErrorMsg) {
-    super(error);
-    this.status = errorCodes[error.code as number];
-  }
-}
-
-export { GeneralError, GoogleError, BadRequest, NotFound, WrongMethod };
+const errorHandler = (
+  err: GeneralError,
+  req: RequestWithContext,
+  res: Response
+) => {
+  errors.report(err);
+  req.context.logger.error(err.message);
+  const statusCode = typeof err.getCode === 'undefined' ? 500 : err.getCode();
+  return res
+    .status(statusCode)
+    .json({ status: statusCode, error: { message: err.message } });
+};
+export { errorHandler };
--- src/middleware/logger/index.ts
+++ src/middleware/logger/index.ts
@@ -1,65 +1,15 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Request, Response } from 'express';
-import { Logging, Log } from '@google-cloud/logging';
-import { Merge } from 'type-fest';
-import { google } from '@google-cloud/logging/build/protos/protos';
+import { Response, NextFunction } from 'express';
+import { RequestWithContext } from '../../interfaces/requestWithContext';
+import Logger from './logger';
 
-const logging = new Logging();
-
-export default class Logger {
-  private log: Log;
-  private requestObj: Request;
-  private defaultMeta: Merge<
-    google.logging.v2.ILogEntry,
-    {
-      timestamp?: google.protobuf.ITimestamp | Date | null;
-      severity?: google.logging.type.LogSeverity | string | null;
-    }
-  >;
-  constructor(logName: string, request: Request) {
-    this.requestObj = request;
-    this.log = logging.log(logName);
-    const traceHeaders = request.header('X-Cloud-Trace-Context') as string;
-    const [trace] = traceHeaders.split('/');
-    this.defaultMeta = {
-      labels: {
-        execution_id: request.header('function-execution-id') as string,
-      },
-      resource: {
-        type: 'cloud_function',
-        labels: {
-          function_name: process.env.FUNCTION_NAME || 'visionapi-dev-endpoint',
-          project_id: process.env.GCP_PROJECT!,
-          region: 'europe-west2',
-        },
-      },
-      trace: `projects/scan2recycle/traces/${trace}`,
-    };
-    const entry = this.log.entry(this.defaultMeta, 'Request Received');
-    this.log.info(entry);
-  }
-
-  response(res: Response, msg: string | Record<string, unknown>) {
-    const metadata = {
-      ...this.defaultMeta,
-      httpRequest: {
-        requestMethod: this.requestObj.method,
-        requestUrl: this.requestObj.url,
-        status: this.requestObj.statusCode,
-        userAgent: this.requestObj.get('user-agent'),
-      },
-    };
-    const entry = this.log.entry(metadata, msg);
-    return this.log.info(entry);
-  }
-
-  info(msg: string | Record<string, unknown>) {
-    const entry = this.log.entry(this.defaultMeta, msg);
-    return this.log.info(entry);
-  }
-
-  error(msg: string | Record<string, unknown>) {
-    const entry = this.log.entry(this.defaultMeta, msg);
-    return this.log.error(entry);
-  }
-}
+export const logger = (logName: string) => (
+  req: RequestWithContext,
+  _res: Response,
+  next: NextFunction
+) => {
+  const logger = new Logger(logName, req);
+  req.context = {
+    logger,
+  };
+  next();
+};
